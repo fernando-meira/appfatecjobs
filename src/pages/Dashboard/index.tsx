a4189326234f7b8a43d8dc3e1e126948a561a710
Fernando Meira <thomwork@outlook.com>
2021-12-08 00:41:42 -0300

    feat: completed layout LargeCarousel home.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import {
   widthPercentageToDP as ww,
@@ -8,36 +8,145 @@ import {
 
 import * as S from './styles';
 
+import { Header } from './components';
+
+interface VacancyProps {
+  tipo: string;
+  titulo: string;
+  id_vaga: number;
+  salario: number;
+  descricao: string;
+  localizacao: string;
+  id_empresa_fk: number;
+}
+
 const Dashboard: React.FC = () => {
-  const batata = [
+  const response: VacancyProps[] = [
     {
-      title: 'First',
+      id_vaga: 21,
+      id_empresa_fk: 21,
+      titulo: 'Desenvolvedor NodeJS',
+      descricao:
+        'É necessário conhecimento em x p t y tecnologias mínimo 5 anos de experiência em desenvolvimento backend com java EE',
+      localizacao: 'Sorocaba-SP',
+      salario: 4500,
+      tipo: 'PJ',
     },
     {
-      title: 'Second',
+      id_vaga: 22,
+      id_empresa_fk: 22,
+      titulo: 'Desenvolvedor Front-end Junior',
+      descricao:
+        'É necessário conhecimento em x p t y tecnologias mínimo 5 anos de experiência em desenvolvimento backend com java EE',
+      localizacao: 'Sorocaba-SP',
+      salario: 4500,
+      tipo: 'PJ',
     },
     {
-      title: 'Thrid',
+      id_vaga: 23,
+      id_empresa_fk: 23,
+      titulo: 'Desenvolvedor NodeJS',
+      descricao:
+        'É necessário conhecimento em x p t y tecnologias mínimo 5 anos de experiência em desenvolvimento backend com java EE',
+      localizacao: 'Sorocaba-SP',
+      salario: 4500,
+      tipo: 'PJ',
+    },
+    {
+      id_vaga: 24,
+      id_empresa_fk: 24,
+      titulo: 'Desenvolvedor NodeJS',
+      descricao:
+        'É necessário conhecimento em x p t y tecnologias mínimo 5 anos de experiência em desenvolvimento backend com java EE',
+      localizacao: 'Sorocaba-SP',
+      salario: 4500,
+      tipo: 'PJ',
+    },
+    {
+      id_vaga: 25,
+      id_empresa_fk: 25,
+      titulo: 'Desenvolvedor NodeJS',
+      descricao:
+        'É necessário conhecimento em x p t y tecnologias mínimo 5 anos de experiência em desenvolvimento backend com java EE',
+      localizacao: 'Sorocaba-SP',
+      salario: 4500,
+      tipo: 'PJ',
+    },
+    {
+      id_vaga: 26,
+      id_empresa_fk: 26,
+      titulo: 'Desenvolvedor NodeJS',
+      descricao:
+        'É necessário conhecimento em x p t y tecnologias mínimo 5 anos de experiência em desenvolvimento backend com java EE',
+      localizacao: 'Sorocaba-SP',
+      salario: 4500,
+      tipo: 'PJ',
     },
   ];
 
-  const renderItems = ({ item, index }) => {
+  const renderItems = ({ item }: { item: VacancyProps }) => {
     return (
-      <View>
-        <Text>{item.title}</Text>
-      </View>
+      <S.CarouselWrapper>
+        <S.CarouselTitle>{item.titulo}</S.CarouselTitle>
+
+        <S.ContentRow>
+          <S.Label>Descrição da vaga: </S.Label>
+          <S.CarouselDescription>{item.descricao}</S.CarouselDescription>
+        </S.ContentRow>
+
+        <S.ContentRow>
+          <S.Label>Localização: </S.Label>
+          <S.RegularText>{item.localizacao}</S.RegularText>
+        </S.ContentRow>
+
+        <S.ContentRow>
+          <S.InfoWrapper>
+            <View>
+              <S.Label>Tipo da vaga: </S.Label>
+              <S.RegularText>{item.tipo}</S.RegularText>
+            </View>
+
+            <View>
+              <S.Label>Salário: </S.Label>
+              <S.SalaryText>{`R$ ${item.salario}`}</S.SalaryText>
+            </View>
+          </S.InfoWrapper>
+        </S.ContentRow>
+
+        <S.Button
+          text="Candidatar-se"
+          style={{ marginTop: 'auto' }}
+          onPress={() => console.log('Candidatar-se')}
+        />
+      </S.CarouselWrapper>
     );
   };
 
   return (
     <S.Container>
+      <Header />
+
+      {/* <S.CarouselWrapper> */}
       <Carousel
         ref={null}
-        data={batata}
-        itemWidth={wh('32')}
+        data={response}
+        itemWidth={wh('40')}
         renderItem={renderItems}
         sliderWidth={ww('100%')}
       />
+      {/* </S.CarouselWrapper> */}
+
+      <S.SmallCarouselWrapper>
+        <Carousel
+          ref={null}
+          data={response}
+          itemWidth={wh('32')}
+          renderItem={renderItems}
+          sliderWidth={ww('100%')}
+        />
+      </S.SmallCarouselWrapper>
+
+      <S.Button text="Ver perfil" />
     </S.Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,25 +1,88 @@
+import styled from 'styled-components/native';
 import {
   widthPercentageToDP as ww,
   heightPercentageToDP as wh,
 } from 'react-native-responsive-screen';
-import styled from 'styled-components/native';
 
 import colors from '~/themes/colors';
+import { DefaultButton } from '~/components';
 
 export const Container = styled.View`
   align-items: center;
-  margin-top: ${wh(1.5)}px;
+  padding: 0 ${wh(4)}px;
+  margin-top: ${wh(3)}px;
 `;
 
-export const ContentWrapper = styled.View`
-  padding-top: ${wh(3)}px;
-`;
-
-export const ImageWrapper = styled.View`
-  z-index: 0;
-  width: 98%;
-`;
-
-export const ButtonImage = styled.TouchableOpacity`
+export const CarouselWrapper = styled.View`
   width: 100%;
+  height: ${wh(52)}px;
+  margin-top: ${wh(4)}px;
+  border-radius: ${ww(4)}px;
+  padding: ${ww(3)}px ${ww(4)}px;
+
+  display: flex;
+  align-items: center;
+  /* justify-content: space-around; */
+
+  background-color: ${colors.secondaryColor};
+`;
+
+export const ContentRow = styled.View`
+  width: 100%;
+
+  margin-top: ${wh(1)}px;
+`;
+
+export const CarouselTitle = styled.Text`
+  margin-top: ${wh(1)}px;
+
+  text-align: center;
+  font-size: ${ww(4)}px;
+  font-family: 'Poppins-SemiBold';
+`;
+
+export const Label = styled.Text`
+  font-size: ${ww(3.5)}px;
+  font-family: 'Poppins-SemiBold';
+`;
+
+export const CarouselDescription = styled.Text`
+  line-height: ${ww(4)}px;
+  font-size: ${ww(3.5)}px;
+  font-family: 'Poppins-Regular';
+`;
+
+export const InfoWrapper = styled.View`
+  width: 100%;
+  margin-top: auto;
+
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  align-self: flex-end;
+  justify-content: space-between;
+`;
+
+export const RegularText = styled.Text`
+  font-size: ${ww(3.5)}px;
+  font-family: 'Poppins-Regular';
+`;
+
+export const SalaryText = styled.Text`
+  font-size: ${ww(3.5)}px;
+  font-family: 'Poppins-SemiBold';
+`;
+
+export const SmallCarouselWrapper = styled.View`
+  width: 100%;
+  height: ${wh(20)}px;
+  margin-top: ${wh(4)}px;
+  border-radius: ${ww(10)}px;
+  padding: ${ww(3)}px ${ww(4)}px;
+
+  background-color: ${colors.secondaryColor};
+`; // TODO alterar esse carrossel para a estizização parecida com o de cima.
+
+export const Button = styled(DefaultButton)`
+  margin-top: ${wh(4)}px;
 `;
