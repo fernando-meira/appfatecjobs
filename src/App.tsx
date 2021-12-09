64d6d89d8e376655e3948bdeb293c30ca52cb0fb
Fernando Meira <thomwork@outlook.com>
2021-12-09 01:35:40 -0300

    feat: created Auth Hook.

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import colors from '~/themes/colors';
 
 import Routes from './routes';
+import { AppProvider } from './hooks';
 
 const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar
-      barStyle="dark-content"
-      showHideTransition="slide"
-      backgroundColor={colors.background}
-    />
-    <View style={{ flex: 1, backgroundColor: colors.background }}>
-      <Routes />
-    </View>
-  </NavigationContainer>
+  <AppProvider>
+    <NavigationContainer>
+      <StatusBar
+        barStyle="dark-content"
+        showHideTransition="slide"
+        backgroundColor={colors.background}
+      />
+      <View style={{ flex: 1, backgroundColor: colors.background }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  </AppProvider>
 );
 
 export default App;
